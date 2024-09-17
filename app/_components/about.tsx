2cb4f7f81504e81fd87427834f67f7683a6faf20
Paulo Araújo <pauloaraujo.phz@gmail.com>
2024-09-17 09:15:56 -0300

    feat: adicionado mensagem no component About

--- app/_components/about.tsx
+++ app/_components/about.tsx
@@ -15,7 +15,32 @@ const About = async () => {
         <DialogHeader>
           <DialogTitle>Sobre o Projeto</DialogTitle>
         </DialogHeader>
-        <DialogDescription></DialogDescription>
+        <DialogDescription>
+          <p>
+            Esse é um projeto fullstack criado com o objetivo de dar visibiliade
+            para os atletas olímpicos e paralímpicos brasileiros. A ideia é
+            listar todos os atletas e suas contas do Instagram, filtrando por
+            nome, modalidade, descrição, seguidores, esportes.
+          </p>
+          <br />
+          <p>
+            Nessa aplicação, foi utilizado dados reais dos atletas brasileiros.
+            Se você encontrou alguma informação incorreta ou sentiu falta de
+            algum atleta, por favor, entre em contato conosco.
+          </p>
+          <br />
+          <p>
+            Confira o código completo no
+            <a
+              href="https://github.com/pauloaraujo028/siga-seu-atleta"
+              target="_blank"
+              className="text-yellow-500 font-bold ml-1"
+            >
+              Repositório do projeto
+            </a>
+            .
+          </p>
+        </DialogDescription>
       </DialogContent>
     </Dialog>
   );
